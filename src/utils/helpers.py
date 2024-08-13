a30c3fa9078ed1db1f0e1ecaf5c9fe6ca6cd869a
jake <jakebonnici2298@gmail.com>
2024-08-13 22:04:18 +0200

    added some helpers.py for querying.

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,5 +1,20 @@
 import pyodbc
+from pyodbc import Row
 
 
 def get_connection(connection_string: str) -> pyodbc.Connection:
     return pyodbc.connect(connection_string)
+
+
+def read_sql_from_file(file_path: str) -> str:
+    with open(file_path, 'r') as file:
+        return file.read()
+
+
+def execute_query(connection: pyodbc.Connection, query: str) -> list[Row]:
+    """return results of the query"""
+    cursor = connection.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
